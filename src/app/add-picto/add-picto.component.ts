e811742fef57c0820fc2f02eb8fabcc571001b30
FabioLousado <f.lousado@polyswantech.com>
2024-02-07 09:26:51 +0100

    ajout de la partie addPicto depuis les fichiers personnels

--- src/app/add-picto/add-picto.component.ts
+++ src/app/add-picto/add-picto.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PictoService } from '../services/picto/picto.service';
+import { Picto } from '../classes/Picto';
 
 @Component({
   selector: 'app-add-picto',
@@ -8,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrl: './add-picto.component.css'
 })
 export class AddPictoComponent {
+
+  picto = new Picto('', '', '');
+
+  constructor(private pictoService : PictoService){}
+
+  addPicto(event : any){
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      const base64String = reader.result as string;
+      this.picto.nom = file.name;
+      this.picto.data = base64String;
+    };
+
+    if(file){
+      reader.readAsDataURL(file);
+    }
+  }
+
+
+  ajouter(){
+    this.pictoService.addPicto(this.picto).then(_ => console.log("Ajout OK"));
+  }
 
 }
--- src/app/intercallaire/intercallaire.component.ts
+++ src/app/intercallaire/intercallaire.component.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { Intercallaire } from '../classes/Intercallaire';
 import { PictoService } from '../services/picto/picto.service';
 import { FormsModule } from '@angular/forms';
+import { AddPictoComponent } from '../add-picto/add-picto.component';
 
 @Component({
   selector: 'app-intercallaire',
   standalone: true,
   templateUrl: './intercallaire.component.html',
-  imports: [CommonModule, PictoComponent, FormsModule],
+  imports: [CommonModule, PictoComponent, FormsModule, AddPictoComponent],
   styleUrls: ['./intercallaire.component.css']
 })
 export class IntercallaireComponent implements OnInit {
@@ -19,6 +20,8 @@ export class IntercallaireComponent implements OnInit {
   search = ""
 
   listPicto : any
+
+  showAddPictoFromFile = false;
 
   constructor(public pictoService: PictoService) { }
 
@@ -37,5 +40,9 @@ export class IntercallaireComponent implements OnInit {
   recherche() {
     this.pictoService.search(this.search);
     this.search = ""
+  }
+
+  addPictoFromFile(){
+    this.showAddPictoFromFile = !this.showAddPictoFromFile
   }
 }
